5fec59b53dfc31f3ca80f7b96809bd7947414e01
Ilker Eker <lkrkr@hotmail.com>
2022-09-16 14:57:25 +0300

    Add project files.

---
+++ RabbitMqProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMqProductAPI.Models;
+using RabbitMqProductAPI.RabitMQ;
+using RabbitMqProductAPI.Services;
+namespace RabitMqProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductservice Productservice;
+        private readonly IRabitMQProducer _rabitMQProducer;
+        public ProductController(IProductservice _Productservice, IRabitMQProducer rabitMQProducer)
+        {
+            Productservice = _Productservice;
+            _rabitMQProducer = rabitMQProducer;
+        }
+        [HttpGet("productlist")]
+        public IEnumerable<Product> ProductList()
+        {
+            var productList = Productservice.GetProductList();
+            return productList;
+        }
+        [HttpGet("getproductbyid")]
+        public Product GetProductById(int Id)
+        {
+            return Productservice.GetProductById(Id);
+        }
+        [HttpPost("addproduct")]
+        public Product AddProduct(Product product)
+        {
+            var productData = Productservice.AddProduct(product);
+            //send the inserted product data to the queue and consumer will listening this data from queue
+            _rabitMQProducer.SendProductMessage(productData);
+            return productData;
+        }
+        [HttpPut("updateproduct")]
+        public Product UpdateProduct(Product product)
+        {
+            return Productservice.UpdateProduct(product);
+        }
+        [HttpDelete("deleteproduct")]
+        public bool DeleteProduct(int Id)
+        {
+            return Productservice.DeleteProduct(Id);
+        }
+    }
+}
---
+++ RabbitMqProductAPI/Migrations/20220727095341_first.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RabbitMqProductAPI.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    ProductName = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    ProductDescription = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    ProductPrice = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    Productstock = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
